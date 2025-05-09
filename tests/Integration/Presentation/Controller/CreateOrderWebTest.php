6f25678d58c41054e747837ae13f6eb8a85ce8c1
Adam Guła <adamdawidgula@gmail.com>
2025-05-09 21:36:47 +0200

    fix and adjust test

--- tests/Integration/Presentation/Controller/CreateOrderWebTest.php
+++ tests/Integration/Presentation/Controller/CreateOrderWebTest.php
@@ -50,12 +50,37 @@ final class CreateOrderWebTest extends WebTestCase
 
         self::assertResponseStatusCodeSame(Response::HTTP_CREATED);
 
-        $createdOrderId = strval(json_decode($this->client->getResponse()->getContent() ?: '', true)['id'] ?? '');
+        $createdOrderId = strval(json_decode($this->client->getResponse()->getContent() ?: '', true)['order']['id'] ?? '');
 
         $order = $this->orderRepository->findById(Ulid::fromString($createdOrderId));
 
         self::assertInstanceOf(Order::class, $order);
         self::assertSame($createdOrderId, $order->getId()->toBase32());
         self::assertCount(2, $order->getItems());
+
+        $expectedResponse = <<<JSON
+            {
+                "order": {
+                    "id": "$createdOrderId",
+                    "status": "new",
+                    "items": [
+                        {
+                            "productId": "1",
+                            "productName": "Product A",
+                            "price": 100,
+                            "quantity": 2
+                        },
+                        {
+                            "productId": "2",
+                            "productName": "Product B",
+                            "price": 50,
+                            "quantity": 1
+                        }
+                    ],
+                    "totalValue": 250
+                }
+            }
+        JSON;
+        self::assertJsonStringEqualsJsonString($expectedResponse, (string) $this->client->getResponse()->getContent());
     }
 }
